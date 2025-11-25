d41401436fd1eab023f47baa121627ed7c468250
Aravinda K M <aravindakm0207@gmail.com>
2025-11-25 16:09:12 +0530

    added events and also did counter example

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -9,5 +9,31 @@ import { Component } from '@angular/core';
 export class Login {
   handleSubmit() {
     alert('kindly login');
+    this.otherFunction();
+  }
+
+  otherFunction() {
+    console.log('function called');
+  }
+
+  sum(a: number, b: number) {
+    console.log(a + b);
+  }
+  count = 0;
+
+  handleCount(val: String) {
+    if (val == 'minus') {
+      this.count = this.count - 1;
+    } else if (val == 'plus') {
+      this.count = this.count + 1;
+    } else {
+      this.count = 0;
+    }
+  }
+
+  handleEvent(event: Event) {
+    console.log('function called', event.type);
+    console.log('value', (event.target as HTMLInputElement).value);
+    console.log('function called', (event.target as Element).classList);
   }
 }
